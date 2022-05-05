c5ea6cc3592fe7a78d1a0074e297ed19721c03fb
oakes <zach@sekao.net>
2022-05-05 16:59:44 +0200

    Use new emscripten module

--- src/web.nim
+++ src/web.nim
@@ -1,34 +1,12 @@
 from nimwave/web/input import nil
+from nimwave/web/emscripten import nil
 from web/core import nil
 import tables, unicode
 
 when not defined(emscripten):
   {.error: "You must set -d:emscripten to build for the web".}
 
-const EM_HTML5_SHORT_STRING_LEN_BYTES = 32
-
-type
-  EmscriptenKeyboardEvent* {.bycopy.} = object
-    timestamp*: cdouble
-    location*: culong
-    ctrlKey*: cint
-    shiftKey*: cint
-    altKey*: cint
-    metaKey*: cint
-    repeat*: cint
-    charCode*: culong
-    keyCode*: culong
-    which*: culong
-    key*: array[EM_HTML5_SHORT_STRING_LEN_BYTES, uint8]
-    code*: array[EM_HTML5_SHORT_STRING_LEN_BYTES, uint8]
-    charValue*: array[EM_HTML5_SHORT_STRING_LEN_BYTES, uint8]
-    locale*: array[EM_HTML5_SHORT_STRING_LEN_BYTES, uint8]
-  em_key_callback_func = proc (eventType: cint, keyEvent: ptr EmscriptenKeyboardEvent, userData: pointer) {.cdecl.}
-
-proc emscripten_set_main_loop(f: proc() {.cdecl.}, a: cint, b: bool) {.importc, header: "<emscripten/emscripten.h>".}
-proc emscripten_set_keydown_callback(target: cstring, userData: pointer, useCapture: bool, callback: em_key_callback_func): cint {.importc, header: "<emscripten/html5.h>".}
-
-proc onKeyDown(eventType: cint, keyEvent: ptr EmscriptenKeyboardEvent, userData: pointer) {.cdecl.} =
+proc onKeyDown(eventType: cint, keyEvent: ptr emscripten.EmscriptenKeyboardEvent, userData: pointer) {.cdecl.} =
   let
     key = $cast[cstring](keyEvent.key.addr)
     keys = key.toRunes
@@ -50,8 +28,8 @@ proc mainLoop() {.cdecl.} =
 
 proc main() =
   core.init()
-  discard emscripten_set_keydown_callback("body", nil, true, onKeyDown)
-  emscripten_set_main_loop(mainLoop, 0, true)
+  discard emscripten.emscripten_set_keydown_callback("body", nil, true, onKeyDown)
+  emscripten.emscripten_set_main_loop(mainLoop, 0, true)
 
 when isMainModule:
   main()
