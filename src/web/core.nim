a0f4d69099d5bb8faa3cc0faa23349628adbbf62
oakes <zach@sekao.net>
2022-05-06 17:25:57 +0200

    Move a few procs in

--- src/web/core.nim
+++ src/web/core.nim
@@ -3,6 +3,8 @@ from ../common import nil
 import unicode
 from nimwave/web import nil
 from nimwave/web/emscripten import nil
+from nimwave/tui/termtools/runewidth import nil
+from strutils import format
 
 proc onKeyPress*(key: iw.Key) =
   common.onKey(key)
@@ -31,6 +33,41 @@ proc onMouseUp*() {.exportc.} =
   info.action = iw.MouseButtonAction.mbaReleased
   common.onMouse(info)
 
+const
+  fontHeight = 20
+  padding = "0.81"
+
+proc charToHtml(ch: iw.TerminalChar, position: tuple[x: int, y: int] = (-1, -1)): string =
+  if cast[uint32](ch.ch) == 0:
+    return ""
+  let
+    fg = web.fgColorToString(ch)
+    bg = web.bgColorToString(ch)
+    additionalStyles =
+      if runewidth.runeWidth(ch.ch) == 2:
+        # add some padding because double width characters are a little bit narrower
+        # than two normal characters due to font differences
+        "display: inline-block; max-width: $1px; padding-left: $2px; padding-right: $2px;".format(fontHeight, padding)
+      else:
+        ""
+    mouseEvents =
+      if position != (-1, -1):
+        "onmousedown='mouseDown($1, $2)' onmousemove='mouseMove($1, $2)'".format(position.x, position.y)
+      else:
+        ""
+  return "<span style='$1 $2 $3' $4>".format(fg, bg, additionalStyles, mouseEvents) & $ch.ch & "</span>"
+
+proc toHtml(tb: iw.TerminalBuffer): string =
+  let
+    termWidth = iw.width(tb)
+    termHeight = iw.height(tb)
+
+  for y in 0 ..< termHeight:
+    var line = ""
+    for x in 0 ..< termWidth:
+      line &= charToHtml(tb[x, y], (x, y))
+    result &= line
+
 proc init*() =
   common.init()
 
@@ -43,6 +80,6 @@ proc tick*() =
     tb = common.tick(termWidth, termHeight)
 
   if lastTb == nil or lastTb[] != tb[]:
-    let html = web.toHtml(tb)
+    let html = toHtml(tb)
     emscripten.setInnerHtml("#content", html)
     lastTb = tb
