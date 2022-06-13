49dae53e341992d781417896e4aed779f25232b5
oakes <zach@sekao.net>
2022-06-13 04:06:03 +0200

    Start work on focus system

--- src/common.nim
+++ src/common.nim
@@ -6,6 +6,9 @@ from strutils import nil
 type
   Platform* = enum
     Tui, Gui, Web,
+  State = object
+    focusIndex*: int
+    focusAreas*: ref seq[iw.TerminalBuffer]
 
 var
   platform*: Platform
@@ -15,8 +18,6 @@ var
   runeQueue: Deque[Rune]
   key: iw.Key
   keyQueue: Deque[iw.Key]
-
-const rollingStone = strutils.splitLines(staticRead("rollingstone.txt"))
 
 proc onMouse*(m: iw.MouseInfo) =
   mouseQueue.addLast(m)
@@ -30,12 +31,16 @@ proc onKey*(k: iw.Key) =
 proc init*() =
   discard
 
-proc scroll(ctx: var nimwave.Context[void], node: JsonNode): nimwave.RenderProc[void] =
+proc addFocusArea(ctx: var nimwave.Context[State]): bool =
+  result = ctx.data.focusIndex == ctx.data.focusAreas[].len
+  ctx.data.focusAreas[].add(ctx.tb)
+
+proc scroll(ctx: var nimwave.Context[State], node: JsonNode): nimwave.RenderProc[State] =
   var
     scrollX = if "scroll-x-start" in node: node["scroll-x-start"].num.int else: 0
     scrollY = if "scroll-y-start" in node: node["scroll-y-start"].num.int else: 0
   return
-    proc (ctx: var nimwave.Context[void], node: JsonNode) =
+    proc (ctx: var nimwave.Context[State], node: JsonNode) =
       let
         width = iw.width(ctx.tb)
         height = iw.height(ctx.tb)
@@ -55,22 +60,23 @@ proc scroll(ctx: var nimwave.Context[void], node: JsonNode): nimwave.RenderProc[
           scrollX = 0
       if "scroll-y-change" in node:
         scrollY += node["scroll-y-change"].num.int
-        let minY = height - iw.height(ctx.tb) + 1
+        let minY = height - iw.height(ctx.tb)
         if minY < 0:
           scrollY = scrollY.clamp(minY, 0)
         else:
           scrollY = 0
 
-proc counter(ctx: var nimwave.Context[void], node: JsonNode): nimwave.RenderProc[void] =
+proc counter(ctx: var nimwave.Context[State], node: JsonNode): nimwave.RenderProc[State] =
   var count = 0
   return
-    proc (ctx: var nimwave.Context[void], node: JsonNode) =
-      proc countBtn(ctx: var nimwave.Context[void], node: JsonNode) =
+    proc (ctx: var nimwave.Context[State], node: JsonNode) =
+      let focused = addFocusArea(ctx)
+      proc countBtn(ctx: var nimwave.Context[State], node: JsonNode) =
         const text = "Count"
         ctx = nimwave.slice(ctx, 0, 0, text.runeLen+2, iw.height(ctx.tb))
         if mouse.action == iw.MouseButtonAction.mbaPressed and iw.contains(ctx.tb, mouse):
           count += 1
-        nimwave.render(ctx, %* {"type": "nimwave.hbox", "border": "single", "children": [text]})
+        nimwave.render(ctx, %* {"type": "nimwave.hbox", "border": "single", "children": [text], "border": if focused: "double" else: "single"})
       ctx.components["count-btn"] = countBtn
       ctx = nimwave.slice(ctx, 0, 0, 20, 3)
       nimwave.render(ctx, %* {"type": "nimwave.hbox", "children": [{"type": "nimwave.vbox", "children": ["", $count]}, {"type": "count-btn"}]})
@@ -80,10 +86,10 @@ type
     text: string
     cursorX: int
 
-proc textField(ctx: var nimwave.Context[void], node: JsonNode, data: ref TextFieldState): nimwave.RenderProc[void] =
+proc textField(ctx: var nimwave.Context[State], node: JsonNode, data: ref TextFieldState): nimwave.RenderProc[State] =
   let id = node["id"].str
   return
-    proc (ctx: var nimwave.Context[void], node: JsonNode) =
+    proc (ctx: var nimwave.Context[State], node: JsonNode) =
       let
         key = iw.Key(node["key"].num.int)
         rune = Rune(node["rune"].num.int)
@@ -112,24 +118,44 @@ proc textField(ctx: var nimwave.Context[void], node: JsonNode, data: ref TextFie
       cell.fg = iw.fgBlack
       ctx.tb[data[].cursorX, 0] = cell
 
-proc tempConverter(ctx: var nimwave.Context[void], node: JsonNode): nimwave.RenderProc[void] =
+proc tempConverter(ctx: var nimwave.Context[State], node: JsonNode): nimwave.RenderProc[State] =
   var data = new TextFieldState
   let comp = textField(ctx, node, data)
   return
-    proc (ctx: var nimwave.Context[void], node: JsonNode) =
+    proc (ctx: var nimwave.Context[State], node: JsonNode) =
       ctx = nimwave.slice(ctx, 0, 0, 10, 3)
       ctx.components["text-field"] = comp
-      nimwave.render(ctx, %* {"type": "nimwave.hbox", "border": "single", "children": [{"type": "text-field", "key": key.ord, "rune": rune.ord}]})
+      let focused = addFocusArea(ctx)
+      nimwave.render(ctx, %* {"type": "nimwave.hbox", "border": if focused: "double" else: "single", "children": [{"type": "text-field", "key": key.ord, "rune": rune.ord}]})
 
-var ctx = nimwave.initContext[void]()
+proc lyrics(ctx: var nimwave.Context[State], node: JsonNode) =
+  const rollingStone = strutils.splitLines(staticRead("rollingstone.txt"))
+  let focused = addFocusArea(ctx)
+  nimwave.render(ctx, %* {"type": "nimwave.vbox", "border": if focused: "double" else: "single", "children": rollingStone})
+
+var ctx = nimwave.initContext[State]()
+new ctx.data.focusAreas
 ctx.statefulComponents["scroll"] = scroll
 ctx.statefulComponents["counter"] = counter
 ctx.statefulComponents["temp-converter"] = tempConverter
+ctx.components["lyrics"] = lyrics
 
 proc tick*(tb: var iw.TerminalBuffer) =
   mouse = if mouseQueue.len > 0: mouseQueue.popFirst else: iw.MouseInfo()
   rune = if runeQueue.len > 0: runeQueue.popFirst else: Rune(0)
   key = if keyQueue.len > 0: keyQueue.popFirst else: iw.Key.None
+
+  let focusChange =
+    case key:
+    of iw.Key.Up:
+      -1
+    of iw.Key.Down:
+      1
+    else:
+      0
+  ctx.data.focusIndex += focusChange
+  ctx.data.focusIndex = min(max(0, ctx.data.focusIndex), ctx.data.focusAreas[].len-1)
+  ctx.data.focusAreas[] = @[]
 
   ctx.tb = tb
   nimwave.render(
@@ -169,7 +195,7 @@ proc tick*(tb: var iw.TerminalBuffer) =
           "children": [
             {"type": "counter", "id": "counter"},
             {"type": "temp-converter", "id": "temp-converter"},
-            rollingStone,
+            {"type": "lyrics"},
           ]
         }
       ,
