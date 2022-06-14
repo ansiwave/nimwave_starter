6c909bd5828679fb2517014f269a3cebdb5db2f5
oakes <zach@sekao.net>
2022-06-14 13:45:29 +0200

    Separate render procs from mount procs

--- src/common.nim
+++ src/common.nim
@@ -49,33 +49,36 @@ type
     scrollX: int
     scrollY: int
 
+proc renderScroll(ctx: var nimwave.Context[State], node: JsonNode, state: ref ScrollState) =
+  let
+    width = iw.width(ctx.tb)
+    height = iw.height(ctx.tb)
+    bounds =
+      if "fit-content" in node and node["fit-content"].bval:
+        (0, 0, -1, -1)
+      else:
+        (0, 0, iw.width(ctx.tb), iw.height(ctx.tb))
+  var ctx = nimwave.slice(ctx, state[].scrollX, state[].scrollY, iw.width(ctx.tb), iw.height(ctx.tb), bounds)
+  nimwave.render(ctx, %* node["child"])
+  if "scroll-x-change" in node:
+    state[].scrollX += node["scroll-x-change"].num.int
+    let minX = width - iw.width(ctx.tb)
+    if minX < 0:
+      state[].scrollX = state[].scrollX.clamp(minX, 0)
+    else:
+      state[].scrollX = 0
+  if "scroll-y-change" in node:
+    state[].scrollY += node["scroll-y-change"].num.int
+    let minY = height - iw.height(ctx.tb)
+    if minY < 0:
+      state[].scrollY = state[].scrollY.clamp(minY, 0)
+    else:
+      state[].scrollY = 0
+
 proc mountScroll(ctx: var nimwave.Context[State], node: JsonNode, state: ref ScrollState): nimwave.RenderProc[State] =
   return
     proc (ctx: var nimwave.Context[State], node: JsonNode) =
-      let
-        width = iw.width(ctx.tb)
-        height = iw.height(ctx.tb)
-        bounds =
-          if "fit-content" in node and node["fit-content"].bval:
-            (0, 0, -1, -1)
-          else:
-            (0, 0, iw.width(ctx.tb), iw.height(ctx.tb))
-      var ctx = nimwave.slice(ctx, state[].scrollX, state[].scrollY, iw.width(ctx.tb), iw.height(ctx.tb), bounds)
-      nimwave.render(ctx, %* node["child"])
-      if "scroll-x-change" in node:
-        state[].scrollX += node["scroll-x-change"].num.int
-        let minX = width - iw.width(ctx.tb)
-        if minX < 0:
-          state[].scrollX = state[].scrollX.clamp(minX, 0)
-        else:
-          state[].scrollX = 0
-      if "scroll-y-change" in node:
-        state[].scrollY += node["scroll-y-change"].num.int
-        let minY = height - iw.height(ctx.tb)
-        if minY < 0:
-          state[].scrollY = state[].scrollY.clamp(minY, 0)
-        else:
-          state[].scrollY = 0
+      renderScroll(ctx, node, state)
 
 proc mountScroll(ctx: var nimwave.Context[State], node: JsonNode): nimwave.RenderProc[State] =
   var state = new ScrollState
@@ -101,72 +104,74 @@ type
     text: string
     cursorX: int
 
+proc renderTextField(ctx: var nimwave.Context[State], node: JsonNode, state: ref TextFieldState, scrollState: ref ScrollState) =
+  let
+    key = if "key" in node: iw.Key(node["key"].num.int) else: iw.Key.None
+    rune = if "rune" in node: Rune(node["rune"].num.int) else: Rune(0)
+  case key:
+  of iw.Key.Backspace:
+    if state[].cursorX > 0:
+      let
+        line = state[].text.toRunes
+        x = state[].cursorX - 1
+        newLine = $line[0 ..< x] & $line[x + 1 ..< line.len]
+      state[].text = newLine
+      state[].cursorX -= 1
+  of iw.Key.Delete:
+    if state[].cursorX < state[].text.runeLen:
+      let
+        line = state[].text.toRunes
+        newLine = $line[0 ..< state[].cursorX] & $line[state[].cursorX + 1 ..< line.len]
+      state[].text = newLine
+  of iw.Key.Left:
+    state[].cursorX -= 1
+    if state[].cursorX < 0:
+      state[].cursorX = 0
+  of iw.Key.Right:
+    state[].cursorX += 1
+    if state[].cursorX > state[].text.runeLen:
+      state[].cursorX = state[].text.runeLen
+  of iw.Key.Home:
+    state[].cursorX = 0
+  of iw.Key.End:
+    state[].cursorX = state[].text.runeLen
+  else:
+    discard
+  if rune.ord >= 32:
+    let
+      line = state[].text.toRunes
+      before = line[0 ..< state[].cursorX]
+      after = line[state[].cursorX ..< line.len]
+    state[].text = $before & $rune & $after
+    state[].cursorX += 1
+  # create scroll component
+  proc mountTextFieldScroll(ctx: var nimwave.Context[State], node: JsonNode): nimwave.RenderProc[State] =
+    return mountScroll(ctx, node, scrollState)
+  ctx.statefulComponents["text-field-scroll"] = mountTextFieldScroll
+  # update scroll position
+  let cursorXDiff = scrollState[].scrollX + state[].cursorX
+  if cursorXDiff >= iw.width(ctx.tb) - 1:
+    scrollState[].scrollX = iw.width(ctx.tb) - 1 - state[].cursorX
+  elif cursorXDiff < 0:
+    scrollState[].scrollX = 0 - state[].cursorX
+  # render
+  ctx = nimwave.slice(ctx, 0, 0, iw.width(ctx.tb), 1)
+  nimwave.render(ctx, %* {
+    "type": "text-field-scroll",
+    "id": "text-field-scroll",
+    "child": state[].text,
+  })
+  if "show-cursor" in node and node["show-cursor"].bval:
+    var cell = ctx.tb[scrollState[].scrollX + state[].cursorX, 0]
+    cell.bg = iw.bgYellow
+    cell.fg = iw.fgBlack
+    ctx.tb[scrollState[].scrollX + state[].cursorX, 0] = cell
+
 proc mountTextField(ctx: var nimwave.Context[State], node: JsonNode, state: ref TextFieldState): nimwave.RenderProc[State] =
-  let id = node["id"].str
   var scrollState = new ScrollState
   return
     proc (ctx: var nimwave.Context[State], node: JsonNode) =
-      let
-        key = if "key" in node: iw.Key(node["key"].num.int) else: iw.Key.None
-        rune = if "rune" in node: Rune(node["rune"].num.int) else: Rune(0)
-      case key:
-      of iw.Key.Backspace:
-        if state[].cursorX > 0:
-          let
-            line = state[].text.toRunes
-            x = state[].cursorX - 1
-            newLine = $line[0 ..< x] & $line[x + 1 ..< line.len]
-          state[].text = newLine
-          state[].cursorX -= 1
-      of iw.Key.Delete:
-        if state[].cursorX < state[].text.runeLen:
-          let
-            line = state[].text.toRunes
-            newLine = $line[0 ..< state[].cursorX] & $line[state[].cursorX + 1 ..< line.len]
-          state[].text = newLine
-      of iw.Key.Left:
-        state[].cursorX -= 1
-        if state[].cursorX < 0:
-          state[].cursorX = 0
-      of iw.Key.Right:
-        state[].cursorX += 1
-        if state[].cursorX > state[].text.runeLen:
-          state[].cursorX = state[].text.runeLen
-      of iw.Key.Home:
-        state[].cursorX = 0
-      of iw.Key.End:
-        state[].cursorX = state[].text.runeLen
-      else:
-        discard
-      if rune.ord >= 32:
-        let
-          line = state[].text.toRunes
-          before = line[0 ..< state[].cursorX]
-          after = line[state[].cursorX ..< line.len]
-        state[].text = $before & $rune & $after
-        state[].cursorX += 1
-      # create scroll component
-      proc mountTextFieldScroll(ctx: var nimwave.Context[State], node: JsonNode): nimwave.RenderProc[State] =
-        return mountScroll(ctx, node, scrollState)
-      ctx.statefulComponents["text-field-scroll"] = mountTextFieldScroll
-      # update scroll position
-      let cursorXDiff = scrollState[].scrollX + state[].cursorX
-      if cursorXDiff >= iw.width(ctx.tb) - 1:
-        scrollState[].scrollX = iw.width(ctx.tb) - 1 - state[].cursorX
-      elif cursorXDiff < 0:
-        scrollState[].scrollX = 0 - state[].cursorX
-      # render
-      ctx = nimwave.slice(ctx, 0, 0, iw.width(ctx.tb), 1)
-      nimwave.render(ctx, %* {
-        "type": "text-field-scroll",
-        "child": state[].text,
-        "id": id & "-scroll",
-      })
-      if "show-cursor" in node and node["show-cursor"].bval:
-        var cell = ctx.tb[scrollState[].scrollX + state[].cursorX, 0]
-        cell.bg = iw.bgYellow
-        cell.fg = iw.fgBlack
-        ctx.tb[scrollState[].scrollX + state[].cursorX, 0] = cell
+      renderTextField(ctx, node, state, scrollState)
 
 proc mountTemperatureTextField(ctx: var nimwave.Context[State], node: JsonNode, state: ref TextFieldState): nimwave.RenderProc[State] =
   let renderTextField = mountTextField(ctx, node, state)
